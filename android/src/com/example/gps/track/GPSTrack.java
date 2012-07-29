ac5eaaf4ed56f7efb5d546f81e60a62da172c7fd
Yi Wang <e@yi-wang.me>
2012-07-29 16:41:14 -0400

    android source works, add .gitignore

--- android/src/com/example/gps/track/GPSTrack.java
+++ android/src/com/example/gps/track/GPSTrack.java
@@ -29,7 +29,7 @@ public class GPSTrack extends DroidGap
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/source/index.html");
     }
 }
 
